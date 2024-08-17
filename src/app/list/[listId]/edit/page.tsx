8c19ead1b6823a23c3a506b45f9aad751a833086
Maxime FERRET <maxime.bounaas@gmail.com>
2024-08-17 23:05:11 +0200

    added import from json feature

--- src/app/list/[listId]/edit/page.tsx
+++ src/app/list/[listId]/edit/page.tsx
@@ -3,7 +3,8 @@
 import { EditableCard } from '@/components/EditableCard'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { List } from '@/lib/types'
+import { Textarea } from '@/components/ui/textarea'
+import { List, QA } from '@/lib/types'
 import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 
@@ -17,6 +18,8 @@ const EditListPage = ({
   const [lists, setLists] = useState<List[]>([])
   const list = lists.find((list: any) => list.id === listId)
 
+  const [jsonData, setJsonData] = useState('')
+
   useEffect(() => {
     if (isMounted.current)
       return localStorage.setItem('lists', JSON.stringify(lists))
@@ -27,6 +30,18 @@ const EditListPage = ({
 
     isMounted.current = true
   }, [listId, lists])
+
+  const updateListName = (name: string) => {
+    if (!list) return
+
+    setLists(prev => [
+      ...prev.filter(list => list.id !== listId),
+      {
+        ...list,
+        name
+      }
+    ])
+  }
 
   const handleAddNewQA = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -52,6 +67,20 @@ const EditListPage = ({
     ])
   }
 
+  const importFromJson = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const data = JSON.parse(jsonData) as QA[]
+    if (!Array.isArray(data)) return
+
+    setLists(prev => [
+      ...prev.filter(list => list.id !== listId),
+      {
+        ...list,
+        data: [...(list?.data ?? []), ...data]
+      } as List
+    ])
+  }
+
   return (
     <div className="p-4 max-w-[800px] mx-auto">
       <Link href="/" className="underline italic text-gray-700 text-sm">
@@ -59,7 +88,12 @@ const EditListPage = ({
       </Link>
 
       <h1 className="text-center font-bold text-2xl mt-2 mb-12">
-        List: {list?.name}
+        List:{' '}
+        <Input
+          type="text"
+          value={list?.name}
+          onChange={e => updateListName(e.target.value)}
+        />
       </h1>
 
       {list?.data?.length === 0 ? (
@@ -94,6 +128,16 @@ const EditListPage = ({
         <Button type="submit" className="w-full p-2 text-white rounded mt-2">
           Add
         </Button>
+      </form>
+
+      <form className="mt-20" onSubmit={importFromJson}>
+        <Textarea
+          placeholder="import from json (id: string, question: string, answer: string{}[])"
+          className="mb-2"
+          value={jsonData}
+          onChange={e => setJsonData(e.target.value)}
+        />
+        <Button className="w-full">Import from JSON</Button>
       </form>
     </div>
   )
